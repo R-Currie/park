aa9f0c897ea087a5c893f727ab29eff087c0cf33
R-Currie <currierobertm@gmail.com>
2019-11-12 21:50:07 -0600

    Fixed some class issues and started media query

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,35 +3,35 @@ import Background from './Background';
 
 const Hero = () => {
     return(
-        <section class="hero">
+        <section className="hero">
             <Background />
-            <div class="hero-content">
+            <div className="hero-content">
             <h1>Macondo National Park</h1>
             <h3>Start Your Adventure</h3>
-            <div class="btns"> 
+            <div className="btns"> 
                 <a href="/hiking" className="btn">
                     <div>
-                        <i class="fa fa-map-o fa-3x"></i>
+                        <i className="fa fa-map-o fa-3x"></i>
                     </div>
-                    <div class="btn-text">Hiking</div>
+                    <div className="btn-text">Hiking</div>
                 </a>
-                <a href="/camping" class="btn">
+                <a href="/camping" className="btn">
                     <div>
-                        <i class="fa fa-leaf fa-3x"></i>
+                        <i className="fa fa-leaf fa-3x"></i>
                     </div>
-                    <div class="btn-text camping">Camping</div>
+                    <div className="btn-text camping">Camping</div>
                 </a>
-                    <a href="/boating" class="btn">
+                    <a href="/boating" className="btn">
                         <div>
-                            <i class="fa fa-life-buoy fa-3x"></i>
+                            <i className="fa fa-life-buoy fa-3x"></i>
                         </div>
-                        <div class="btn-text boating">Boating</div>
+                        <div className="btn-text boating">Boating</div>
                     </a>
-                    <a href="/biking" class="btn">
+                    <a href="/biking" className="btn">
                         <div>
-                            <i class="fa fa-bicycle fa-3x"></i>
+                            <i className="fa fa-bicycle fa-3x"></i>
                         </div>
-                        <div class="btn-text">Biking</div>
+                        <div className="btn-text">Biking</div>
                     </a>
                 </div>
             </div>
