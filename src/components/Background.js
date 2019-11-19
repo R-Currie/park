0ffa01860ecbc85e67083f0e3e496a4823ee100f
R-Currie <currierobertm@gmail.com>
2019-11-18 20:32:00 -0600

    Undid clicking outside the modal

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,7 +3,7 @@ import "./app.css"
 
 const Background = () => {
     return (
-        <a className="background-a" href="/"><div className="background"></div></a>
+        <div className="background"></div>
     )
 }
 
